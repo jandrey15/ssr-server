80d7dc0e0a8f28872a726e563066d418d5c369dd
John Serrano <andrey_201015@hotmail.com>
2020-02-03 16:16:32 -0500

    Add authentication with twitter

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const passport = require("passport");
+const session = require("express-session")
 const boom = require("@hapi/boom");
 const cookieParser = require("cookie-parser");
 const axios = require("axios");
@@ -12,11 +13,19 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+// Todo esto es para poder usar la libreria de passport-twitter
+app.use(session({ secret: config.sessionSecret }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 //  Basic strategy
 require("./utils/auth/strategies/basic");
 
 // OAuth strategy
 require("./utils/auth/strategies/oauth");
+
+// Twitter strategy
+require("./utils/auth/strategies/twitter");
 
 app.post("/auth/sign-in", async function(req, res, next) {
   passport.authenticate("basic", function(error, data) {
@@ -136,6 +145,27 @@ app.get(
 app.get(
   "/auth/google-oauth/callback",
   passport.authenticate("google-oauth", { session: false }),
+  function(req, res, next) {
+    if (!req.user) {
+      next(boom.unauthorized());
+    }
+
+    const { token, ...user } = req.user;
+
+    res.cookie("token", token, {
+      httpOnly: !config.dev,
+      secure: !config.dev
+    });
+
+    res.status(200).json(user);
+  }
+);
+
+app.get("/auth/twitter", passport.authenticate("twitter"));
+
+app.get(
+  "/auth/twitter/callback",
+  passport.authenticate("twitter", { session: false }),
   function(req, res, next) {
     if (!req.user) {
       next(boom.unauthorized());
